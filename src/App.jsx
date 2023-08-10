6f69209d8ffc7eebcc031dba85f8cbd37f47e592
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-08-10 01:05:23 -0400

    Se crearon las páginas de la parte publica del sistema

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import AuthLayout from './layouts/AuthLayout'
+import Login from './paginas/Login'
+import Registrar from './paginas/Registrar'
+import OlvidePassword from './paginas/OlvidePassword'
+import NuevoPassword from './paginas/NuevoPassword'
+import ConfirmarCuenta from './paginas/ConfirmarCuenta'
 
 function App() {
   return (
-    <h1>Ups Task MERN</h1>
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<AuthLayout />}>
+              <Route index element={<Login />} />
+              <Route path="registrar" element={<Registrar />} />
+              <Route path="olvide-password" element={<OlvidePassword />} />
+              <Route path="olvide-password/:token" element={<NuevoPassword />} />
+              <Route path="confirmar/:id" element={<ConfirmarCuenta />} />
+          </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
