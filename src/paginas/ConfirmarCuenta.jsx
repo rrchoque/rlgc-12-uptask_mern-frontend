8d189c5b27fc32b830925bc2d2216ddc34210178
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-08-11 19:36:02 -0400

    Se crearon las plantillas de las vistas de registro, validación y cambio de contraseña

--- src/paginas/ConfirmarCuenta.jsx
+++ src/paginas/ConfirmarCuenta.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const ConfirmarCuenta = () => {
   return (
-    <div>ConfirmarCuenta</div>
+    <>
+        <h1 className="text-sky-600 font-black text-6xl capitalize">Confirma tu cuenta y Comienza a crear tus {''}
+            <span className="text-slate-700">proyectos</span>
+        </h1>
+
+        <div className='mt-20 md:mt-10 shadow-lg px-5 py-10 rounded-xl bg-white'>
+
+
+            <Link 
+                className='block text-center my-5 text-slate-500 uppercase text-sm'
+                to="/"
+            >Inicia Sesión</Link>
+        </div>
+    </>
   )
 }
 
--- src/paginas/NuevoPassword.jsx
+++ src/paginas/NuevoPassword.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NuevoPassword = () => {
   return (
-    <div>NuevoPassword</div>
+    <>
+        <h1 className="text-sky-600 font-black text-6xl capitalize">Reestablece tu password y no pierdas acceso a tus {''}
+            <span className="text-slate-700">proyectos</span>
+        </h1>
+            <form 
+                className="my-10 bg-white shadow rounded-lg p-10"
+            >
+                
+                <div className="my-5">
+                    <label 
+                        className="uppercase text-gray-600 block text-xl font-bold"
+                        htmlFor="password"
+                    >Nuevo Password</label>
+                    <input
+                        id="password"
+                        type="password"
+                        placeholder="Escribe tu Nuevo Password"
+                        className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+                    />
+                </div>
+                <input 
+                    type="submit"
+                    value="Guardar Nuevo Password"
+                    className="bg-sky-700 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors"
+                />
+                
+            </form>
+
+            <Link
+                className='block text-center my-5 text-slate-500 uppercase text-sm'
+                to="/"
+            >Inicia Sesión</Link>
+    </>
   )
 }
 
