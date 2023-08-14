a81f7e64677d05aa97072edc0588c38fdf9ada82
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-08-14 01:18:22 -0400

    Se agregó la ruta del API a las variables de entorno

--- src/paginas/Registrar.jsx
+++ src/paginas/Registrar.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import Alerta from '../components/Alerta'
+import clienteAxios from '../config/clienteAxios'
 
 const Registrar = () => {
     const [ nombre, setNombre ] = useState('')
@@ -39,6 +40,24 @@ const Registrar = () => {
         setAlerta({})
 
         // Crear el usuario en la API
+        try {
+            const { data } = await clienteAxios.post(`/usuarios`, {nombre, email, password} )
+
+            setAlerta({
+                msg: data.msg,
+                error: false
+            })
+
+            setNombre('')
+            setEmail('')
+            setPassword('')
+            setRepetirPassword('')
+        } catch (error) {
+            setAlerta({
+                msg: error.response.data.msg,
+                error: true
+            })
+        }
 
     }
 
