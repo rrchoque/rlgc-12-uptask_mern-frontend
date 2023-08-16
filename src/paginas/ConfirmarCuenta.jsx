59710a861d34496361a95592d1468f7d82411101
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-08-16 00:31:15 -0400

    Se agregó la funcionalidad para confirmar la cuenta mediante token

--- src/paginas/ConfirmarCuenta.jsx
+++ src/paginas/ConfirmarCuenta.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import clienteAxios from '../config/clienteAxios'
+import Alerta from '../components/Alerta'
 
 const ConfirmarCuenta = () => {
+
+  const [alerta, setAlerta] = useState({})
+  const [cuentaConfirmada, setCuentaConfirmada] = useState(false)
+
+  const params = useParams();
+  const { id } = params
+
+  useEffect(() => {
+    const confirmarCuenta = async () => {
+      try {
+          const url = `/usuarios/confirmar/${id}`
+          const { data } = await clienteAxios(url)
+
+          setAlerta({
+            msg: data.msg,
+            error: false
+          })
+          setCuentaConfirmada(true)
+
+      } catch (error) {
+          setAlerta({
+            msg: error.response.data.msg,
+            error: true
+          })
+      }
+    }
+    confirmarCuenta();
+  }, [])
+
+  const { msg } = alerta
+
   return (
     <>
         <h1 className="text-sky-600 font-black text-6xl capitalize">Confirma tu cuenta y Comienza a crear tus {''}
@@ -9,12 +42,14 @@ const ConfirmarCuenta = () => {
         </h1>
 
         <div className='mt-20 md:mt-10 shadow-lg px-5 py-10 rounded-xl bg-white'>
+          {msg && <Alerta alerta={alerta} />}
 
-
+          {cuentaConfirmada && (
             <Link 
                 className='block text-center my-5 text-slate-500 uppercase text-sm'
                 to="/"
             >Inicia Sesión</Link>
+          )}
         </div>
     </>
   )
