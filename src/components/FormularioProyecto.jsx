312b3901b36a00acf9236edc1c56ffc05da6d587
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-08-22 00:05:25 -0400

    Se creó la funcionalidad para crear proyectos

--- src/components/FormularioProyecto.jsx
+++ src/components/FormularioProyecto.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react"
+import useProyectos from "../hooks/useProyectos"
+import Alerta from './Alerta'
 
 const FormularioProyecto = () => {
 
@@ -7,10 +9,37 @@ const FormularioProyecto = () => {
     const [fechaEntrega, setFechaEntrega] = useState('')
     const [cliente, setCliente] = useState('')
 
+    const { mostrarAlerta, alerta, submitProyecto } = useProyectos();
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+
+        if([nombre, descripcion, fechaEntrega, cliente].includes('') ) {
+            mostrarAlerta({
+                msg: 'Todos los Campos son Obligatorios',
+                error: true
+            })
+
+            return
+        }
+
+        // Pasar los datos hacia el provider
+        await submitProyecto({ nombre, descripcion, fechaEntrega, cliente})
+
+        setNombre('')
+        setDescripcion('')
+        setFechaEntrega('')
+        setCliente('')
+    }
+
+    const { msg } = alerta
+
     return (
             <form 
                 className="bg-white py-10 px-5 md:w-1/2 rounded-lg shadow"
+                onSubmit={handleSubmit}
             >
+                {msg && <Alerta alerta={alerta} />}
                     <div className='mb-5'>
                         <label
                             className="text-gray-700 uppercase font-bold text-sm"
--- src/context/ProyectosProvider.jsx
+++ src/context/ProyectosProvider.jsx
@@ -1,15 +1,60 @@
 import { useState, useEffect, createContext } from 'react'
 import clienteAxios from '../config/clienteAxios'
+import { useNavigate } from 'react-router-dom'
 
 const ProyectosContext = createContext();
 
 const ProyectosProvider = ({children}) => {
     const [proyectos, setProyectos] = useState([]);
+    const [alerta, setAlerta] = useState({});
+
+    const navigate = useNavigate();
+
+    const mostrarAlerta = alerta => {
+        setAlerta(alerta)
+
+        setTimeout(() => {
+            setAlerta({})
+        }, 5000);
+    }
+
+    const submitProyecto = async proyecto => {
+        try {
+            const token = localStorage.getItem('token')
+            if(!token) return
+
+            const config = {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`
+                }
+            }
+
+            const { data } = await clienteAxios.post('/proyectos', proyecto, config)
+
+            setProyectos([...proyectos, data])
+
+            setAlerta({
+                msg: 'Proyecto Creado Correctamente',
+                error: false
+            })
+
+            setTimeout(() => {
+                setAlerta({})
+                navigate('/proyectos')
+            }, 3000);
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
     return (
         <ProyectosContext.Provider
             value={{
-                proyectos
+                proyectos,
+                mostrarAlerta,
+                alerta,
+                submitProyecto
             }}
         >{children}
         </ProyectosContext.Provider>
