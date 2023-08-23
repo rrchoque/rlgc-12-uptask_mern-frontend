458a62e4d6f1a2375d0889d218808fd2396600be
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-08-22 23:36:03 -0400

    Se creó la funcionalidad de eliminación de proyectos

--- src/context/ProyectosProvider.jsx
+++ src/context/ProyectosProvider.jsx
@@ -142,6 +142,38 @@ const ProyectosProvider = ({children}) => {
         }
     }
 
+    const eliminarProyecto = async id => {
+        try {
+            const token = localStorage.getItem('token')
+            if(!token) return
+
+            const config = {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`
+                }
+            }
+
+            const { data } = await clienteAxios.delete(`/proyectos/${id}`, config)
+
+            // Sincronizar el state
+            const proyectosActualizados = proyectos.filter(proyectoState => proyectoState._id !== id )
+            setProyectos(proyectosActualizados)
+
+            setAlerta({
+                msg: data.msg,
+                error: false
+            })
+
+            setTimeout(() => {
+                setAlerta({})
+                navigate('/proyectos')
+            }, 3000);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
         <ProyectosContext.Provider
             value={{
@@ -151,7 +183,8 @@ const ProyectosProvider = ({children}) => {
                 submitProyecto,
                 obtenerProyecto,
                 proyecto,
-                cargando
+                cargando,
+                eliminarProyecto
             }}
         >{children}
         </ProyectosContext.Provider>
