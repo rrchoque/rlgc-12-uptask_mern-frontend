e9da2bdbe1342daffc56649d70d4dd560aa649a4
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-09-03 01:13:13 -0400

    Se agregó redireccionamiento a proyectos cuando se quiere ingresar a una ruta de login cuando ya existe token

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,14 +1,22 @@
-import { Outlet } from 'react-router-dom'
+import { Navigate, Outlet } from 'react-router-dom'
+import useAuth from '../hooks/useAuth';
 const AuthLayout = () => {
-  return (
-    <>
-        <main className='container mx-auto mt-5 md:mt-20 p-5 md:flex md:justify-center'>
-            <div className='md:w-2/3 lg:w-2/5 '>
-                <Outlet />
-            </div>
-        </main>
-    </>
-  )
+
+    const { auth, cargando } = useAuth();
+    if(cargando) return 'Cargando...'
+    return (
+        <>
+            {auth._id ? 
+            <Navigate to="/proyectos" /> : (
+              <main className='container mx-auto mt-5 md:mt-20 p-5 md:flex md:justify-center'>
+                  <div className='md:w-2/3 lg:w-2/5 '>
+                      <Outlet />
+                  </div>
+              </main>
+            )}
+
+        </>
+    )
 }
 
 export default AuthLayout
